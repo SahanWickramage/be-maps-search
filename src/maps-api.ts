0c5099af330621e2b1a6365a4072050f6e08d5f7
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-09-20 12:08:15 +1000

    refactored code to filter au only responses

--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,17 +1,21 @@
 import axios from 'axios'
 import { Result, TomTomResponse } from './type/maps-api';
 
+const countryCode: string = 'AU';
+
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
-export async function getPlaceAutocomplete(key: string, address: string, country: string = 'AU'): Promise<Result[]> {
+export async function getPlaceAutocomplete(key: string, address: string): Promise<Result[]> {
     const autocomplete = await axios.get<TomTomResponse>(`https://api.tomtom.com/search/2/search/${address}.json'`, {
         params: {
             key: key,
             limit: 10, // change limit accordingly
-            countrySet: country, // 'Australia didn't work properly, hence set 'AU'
+            countrySet: countryCode, // 'Australia' didn't work properly, hence set 'AU'
             typeahead: true
         }
     });
-    return autocomplete.data.results.map((result) => ({
+    return autocomplete.data.results
+    .filter(result => result.address?.countryCode === countryCode)
+    .map((result) => ({
         ...result
     }))
 }
